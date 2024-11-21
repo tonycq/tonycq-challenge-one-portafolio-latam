7494e8c8d32c190666d3b4f4fbc62f31883ff840
tonycq <anthonycq2020@gmail.com>
2024-11-21 11:43:13 -0300

    correcion de enlaces <a> construccion de footer en proceso

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dropdownContent = document.querySelector('.navegacion__lista')
 const salirButton = document.querySelector('#salir')
 
 
+
 // Función que alterna la visibilidad del contenido del dropdown
 function toggleDropdown() {
     const estaExpandido =dropdownContent.classList.toggle('show');
@@ -13,7 +14,7 @@ function toggleDropdown() {
 // Función para cerrar el dropdown
 function closeDropdown() {
     dropdownContent.classList.remove('show');
-    // cierra aria-expandedcuando sea  falso 
+    // cierra aria-expanded cuando sea  falso 
     dropdownButton.setAttribute('aria-expanded', 'false');
 }
 
@@ -24,9 +25,19 @@ dropdownButton.addEventListener('click',toggleDropdown);
 // Añadir el evento de click al botón "Salir"
 salirButton.addEventListener('click', closeDropdown);
 
+// Selecciona todos los enlaces dentro del menú de navegación
+const menuLinks = document.querySelectorAll('.navegacion__elemento a');
+
+// Añadir un evento de clic a cada enlace
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        // Cerrar el menú cuando se haga clic en un enlace
+        closeDropdown();
+    });
+});
 
 
-// Selecciona todos los enlaces y el botón del menú
+// Selecciona todos los elementos de la lista de navegación (enlaces y botones)
 const items = document.querySelectorAll('.navegacion__elemento a, .navegacion__elemento button');
 
 // Añade un evento de clic a cada elemento (enlace o botón)
